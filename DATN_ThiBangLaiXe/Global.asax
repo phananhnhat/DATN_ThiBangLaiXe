c5a97973d15a0d73a1bf8a45f6b0e414827c2251
phananhnhat <anhnhatphan1995@gmail.com>
2018-01-29 01:23:01 +0700

    NhatPA: Khởi tạo Project 28/1/2018

---
+++ DATN_ThiBangLaiXe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DATN_ThiBangLaiXe.MvcApplication" Language="C#" %>
---
+++ DATN_ThiBangLaiXe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DATN_ThiBangLaiXe.Startup))]
+namespace DATN_ThiBangLaiXe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
